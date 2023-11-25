9371a6f296800a63b6767599988c80a7268fc505
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2023-11-25 21:18:03 +0500

    test

--- src/components/News.js
+++ src/components/News.js
@@ -5,31 +5,81 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: true
+      loading: false,
+      page: 1
     }
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=8f266b6675504f33982bdb24bf0ec259";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=8f266b6675504f33982bdb24bf0ec259&page=1&pageSize=18";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles })
+
+    // Filter out articles with titles containing the word "removed"
+    const filteredArticles = parsedData.articles.filter(element => !element.title.toLowerCase().includes("removed"));
+
+    this.setState({
+      articles: filteredArticles,
+      totalResults: filteredArticles.length,
+    })
   }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=8f266b6675504f33982bdb24bf0ec259&page=${this.state.page - 1}&pageSize=18`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    // Filter out articles with titles containing the word "removed"
+    const filteredArticles = parsedData.articles.filter(element => !element.title.toLowerCase().includes("removed"));
+
+    this.setState({
+      articles: filteredArticles,
+      page: this.state.page - 1,
+    })
+  }
+
+  handleNextClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=8f266b6675504f33982bdb24bf0ec259&page=${this.state.page + 1}&pageSize=18`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    // Filter out articles with titles containing the word "removed"
+    const filteredArticles = parsedData.articles.filter(element => !element.title.toLowerCase().includes("removed"));
+
+    this.setState({
+      articles: filteredArticles,
+      page: this.state.page + 1,
+    })
+
+  }
+
   render() {
     return (
-      <>
-        <div className="container my-3">
-          <h1>News Monkey - Top Headlines</h1>
-          <div className="row">
-            {this.state.articles.map((element) => {
-              return <div className="col-md-4" key={element.url}>
-                <NewsItem key={element.url} title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageurl={element.urlToImage} newsUrl={element.url} />
+      <div className="container my-3">
+        <h1 className="text-center">News Monkey - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles
+            .filter(element => !element.title.toLowerCase().includes("removed"))
+            .map(element => (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  key={element.url}
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageurl={element.urlToImage}
+                  newsUrl={element.url}
+                />
               </div>
-            })}
-          </div>
+            ))}
         </div>
-      </>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button disabled={this.state.page > Math.ceil(this.state.totalResults / 20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
     )
   }
 }
